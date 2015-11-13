5ca023b2695cf4d6c8bdaf184846ae4608dafcaa
Подивилов Евгений <Evgeniy.Podivilov@chelpipe.ru>
2015-11-13 14:49:27 +0500

    Multi-user alfa-version Tic-tac-toe game is completed.
    You can specify the names of the players.
    If the names are the same or empty, then the names are chosen by default
    
    Button new game clears scores and it allows you to specify new names players.
    
    At the end of the round are invited to play or start a new

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,15 +1,27 @@
 var steps = ["×", "○"];
+var gameField = document.getElementsByClassName("flex-game-field");
+var cell = [];
+var score1 = 0;
+var score2 = 0;
+var player1 = document.getElementById("player1");
+var player2 = document.getElementById("player2");
+var winner = document.getElementById("winner");
 
-function clearGameBoard() {
+function clearGameBoard(clearScore) {
 
-    var gameField = document.getElementsByClassName("flex-game-field");
-    Array.prototype.forEach.call(gameField, function (field) {
+    if(clearScore) {
+        score1 = 0;
+        score2 = 0;
+    }
+
+    Array.prototype.forEach.call(gameField, function (field, id) {
+        cell[id] = field;
         field.innerHTML = "";
-    })
-
-    document.getElementById("score1").innerHTML = 0;
-    document.getElementById("score2").innerHTML = 0;
-    document.getElementById("curentMove").innerHTML = steps[1];
+    });
+    //Debug information who curent move
+    //document.getElementById("curentMove").innerHTML = steps[1];
+    document.getElementById("score1").innerHTML = score1;
+    document.getElementById("score2").innerHTML = score2;
 }
 
 function addEvent(element, eventName, callback) {
@@ -20,20 +32,67 @@ function addEvent(element, eventName, callback) {
     }
 }
 
+function checkVictory() {
+    if((cell[0].innerHTML == cell[1].innerHTML) && (cell[1].innerHTML == cell[2].innerHTML)) { return cell[0].innerHTML; }
+    if((cell[3].innerHTML == cell[4].innerHTML) && (cell[4].innerHTML == cell[5].innerHTML)) { return cell[3].innerHTML; }
+    if((cell[6].innerHTML == cell[7].innerHTML) && (cell[7].innerHTML == cell[8].innerHTML)) { return cell[6].innerHTML; }
+    if((cell[0].innerHTML == cell[3].innerHTML) && (cell[3].innerHTML == cell[6].innerHTML)) { return cell[0].innerHTML; }
+    if((cell[1].innerHTML == cell[4].innerHTML) && (cell[4].innerHTML == cell[7].innerHTML)) { return cell[1].innerHTML; }
+    if((cell[2].innerHTML == cell[5].innerHTML) && (cell[5].innerHTML == cell[8].innerHTML)) { return cell[2].innerHTML; }
+    if((cell[6].innerHTML == cell[4].innerHTML) && (cell[4].innerHTML == cell[2].innerHTML)) { return cell[6].innerHTML; }
+    if((cell[0].innerHTML == cell[4].innerHTML) && (cell[4].innerHTML == cell[8].innerHTML)) { return cell[0].innerHTML; }
+    if(cell.every(function(element, id, array) { return (array[id].innerHTML != ""); })) { return "Draw"; }
+    return false;
+}
+
 document.body.onload = function() {
+
     addEvent(document.getElementById("newGame"), "click", function() {
-        clearGameBoard();
+        clearGameBoard(true);
+        var playername1 = document.getElementById("playername1").value;
+        var playername2 = document.getElementById("playername2").value;
+        if( (playername1 != '') && (playername2 != '') && (playername1 != playername2) ) {
+            player1.innerHTML = document.getElementById("playername1").value;
+            player2.innerHTML = document.getElementById("playername2").value;
+        }
     });
 
     addEvent(document.getElementById("game_board"), "click", function(event) {
-        if(event.target && !document.getElementById(event.target.id).innerHTML) {
-            var curentMove = steps.pop();
-            steps.unshift(curentMove);
-            document.getElementById("curentMove").innerHTML = steps[1];
-            document.getElementById(event.target.id).innerHTML = curentMove;
+        if(event.target && event.target.className == 'flex-game-field') {
+            if(!document.getElementById(event.target.id).innerHTML) {
+                var curentMove = steps.pop();
+                steps.unshift(curentMove);
+                //Debug information who curent move
+                //document.getElementById("curentMove").innerHTML = steps[1];
+                document.getElementById(event.target.id).innerHTML = curentMove;
+                var isVictory = checkVictory();
+                if(isVictory != "") {
+                    switch (isVictory) {
+                        case '×':
+                            score1 += 2;
+                            winner.innerHTML = player1.innerHTML + " wins!";
+                            winner.className = winner.className + " alert-success";
+                            break;
+                        case '○':
+                            score2 += 2;
+                            winner.innerHTML = player2.innerHTML + " wins!";
+                            winner.className = winner.className + " alert-success";
+                            break;
+                        default:
+                            score1 += 1;
+                            score2 += 1;
+                            winner.innerHTML = "Draw";
+                            winner.className = winner.className + " alert-warning";
+                    }
+                    document.getElementById("score1").innerHTML = score1;
+                    document.getElementById("score2").innerHTML = score2;
+                    $('#resultModal').modal('show');
+                    clearGameBoard(false);
+                }
+            }
         }
-    })
+    });
 
-    clearGameBoard();
+    $('#newGameModal').modal('show');
 }
 
